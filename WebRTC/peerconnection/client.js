f48ae9fbd4353d7fc7e199971de9de7eecf36f27
Juan Navarro <juan.navarro@gmx.es>
2020-09-22 17:39:02 +0200

    webrtc/peerconnection: add WebRTC statistics retrieval

--- WebRTC/peerconnection/client.js
+++ WebRTC/peerconnection/client.js
@@ -6,6 +6,7 @@
 const global = {
   pcSend: null,
   pcRecv: null,
+  statsInterval: null,
 };
 
 // HTML UI elements
@@ -14,13 +15,16 @@ const global = {
 const ui = {
   // Inputs
   startWebrtc: document.getElementById("uiStartWebrtc"),
+  stopWebrtc: document.getElementById("uiStopWebrtc"),
 
   // Video
   localVideo: document.getElementById("uiLocalVideo"),
   remoteVideo: document.getElementById("uiRemoteVideo"),
 };
 
+ui.stopWebrtc.disabled = true;
 ui.startWebrtc.addEventListener("click", startWebrtc);
+ui.stopWebrtc.addEventListener("click", stopWebrtc);
 
 window.addEventListener("load", () => {
   console.log("[on window.load] Page loaded");
@@ -41,8 +45,8 @@ window.addEventListener("beforeunload", () => {
   console.log("[on window.beforeunload] Page unloading");
 });
 
-// startWebrtc() implementation
-// ============================
+// START implementation
+// ====================
 
 async function startWebrtc() {
   // RTCPeerConnection setup.
@@ -54,8 +58,12 @@ async function startWebrtc() {
   // Media flow.
   await startWebrtcMedia();
 
+  // Statistics.
+  await startWebrtcStats();
+
   // Update UI.
   ui.startWebrtc.disabled = true;
+  ui.stopWebrtc.disabled = false;
 }
 
 function startWebrtcPc() {
@@ -164,4 +172,114 @@ async function startWebrtcMedia() {
       `[pcSend.addTrack] kind: ${track.kind}, direction: ${tc.direction}`
     );
   }
+}
+
+async function startWebrtcStats() {
+  // Program regular retrieval of stats.
+  const intervalID = setInterval(async () => {
+    const pcSend = global.pcSend;
+    const pcRecv = global.pcRecv;
+
+    // getStats() returns RTCStatsReport, which behaves like a Map.
+    // Each RTCStatsReport's value is an RTCStats-derived object, where "type"
+    // is one of the RTCStatsType enum.
+    //
+    // Spec:
+    // https://w3c.github.io/webrtc-pc/#dom-rtcstatsreport
+    // https://w3c.github.io/webrtc-pc/#dom-rtcstats
+    // https://w3c.github.io/webrtc-stats/#dom-rtcstatstype
+    const [reportSend, reportRecv] = await Promise.all([
+      pcSend.getStats(),
+      pcRecv.getStats(),
+    ]);
+
+    // DEBUG
+    reportSend.forEach((stat) => console.log(JSON.stringify(stat)));
+
+    // Extract values to an Array, so map(), filter(), etc are available.
+    const statsSend = Array.from(reportSend.values());
+    const statsRecv = Array.from(reportRecv.values());
+
+    // Obtain all needed stats, finding them by their type.
+    const rtpOutVideos = statsSend.filter(
+      (s) => s.type === "outbound-rtp" && s.kind === "video"
+    );
+    const rtpRemoteInVideos = statsSend.filter(
+      (s) => s.type === "remote-inbound-rtp" && s.kind === "video"
+    );
+    const candidatePairs = statsSend.filter((s) => s.type === "candidate-pair");
+    const localCandidates = statsSend.filter(
+      (s) => s.type === "local-candidate"
+    );
+    const remoteCandidates = statsSend.filter(
+      (s) => s.type === "remote-candidate"
+    );
+    const codecs = statsSend.filter((s) => s.type === "codec");
+    const transports = statsSend.filter((s) => s.type === "transport");
+
+    // Filter and match stats, to find the wanted values.
+    // (report only from first video track that is found)
+    const rtpOutVideo = rtpOutVideos[0];
+    const rtpRemoteInVideo = rtpRemoteInVideos.find(
+      (r) => r.id === rtpOutVideo.remoteId
+    );
+    const codec = codecs.find((c) => c.id === rtpOutVideo.codecId);
+    const transport = transports.find((t) => t.id === rtpOutVideo.transportId);
+    const candidatePair = candidatePairs.find(
+      (p) => p.id === transport.selectedCandidatePairId
+    );
+    const localCandidate = localCandidates.find(
+      (c) => c.id === candidatePair.localCandidateId
+    );
+    const remoteCandidate = remoteCandidates.find(
+      (c) => c.id === candidatePair.remoteCandidateId
+    );
+
+    let data = {};
+    data.localSsrc = rtpOutVideo.ssrc;
+    data.remoteSsrc = rtpRemoteInVideo.ssrc;
+    data.codec = codec.mimeType;
+    data.localPort = localCandidate.port;
+    data.remotePort = remoteCandidate.port;
+    data.packetsSent = rtpOutVideo.packetsSent;
+    data.retransmittedPacketsSent = rtpOutVideo.retransmittedPacketsSent;
+    data.bytesSent = rtpOutVideo.bytesSent;
+    data.nackCount = rtpOutVideo.nackCount;
+    data.firCount = rtpOutVideo.firCount ? rtpOutVideo.firCount : 0;
+    data.pliCount = rtpOutVideo.pliCount ? rtpOutVideo.pliCount : 0;
+    data.sliCount = rtpOutVideo.sliCount ? rtpOutVideo.sliCount : 0;
+    data.iceRoundTripTime = candidatePair.currentRoundTripTime;
+    data.inBitrate = candidatePair.availableIncomingBitrate
+      ? candidatePair.availableIncomingBitrate
+      : 0;
+    data.outBitrate = candidatePair.availableOutgoingBitrate
+      ? candidatePair.availableOutgoingBitrate
+      : 0;
+
+    // Use all obtained stats to print their values.
+    console.log("[on interval] SEND VIDEO STATS:", data);
+  }, 3000);
+  global.statsInterval = intervalID;
+}
+
+// STOP implementation
+// ===================
+
+function stopWebrtc() {
+  ui.localVideo.pause();
+  ui.localVideo.srcObject = null;
+  ui.remoteVideo.pause();
+  ui.remoteVideo.srcObject = null;
+
+  clearInterval(global.statsInterval);
+  global.pcSend.close();
+  global.pcSend = null;
+  global.pcRecv.close();
+  global.pcRecv = null;
+
+  // Update UI.
+  ui.startWebrtc.disabled = false;
+  ui.stopWebrtc.disabled = true;
+
+  console.log("[stopWebrtc] Stopped");
 }
